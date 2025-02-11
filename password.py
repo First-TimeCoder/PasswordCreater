cf1dc3cc4bf5a8879a032f828c3606b81783c1d9
First-TimeCoder ! <xuminxia.no2@gmail.com>
2025-02-11 20:00:42 +0000

    password

---
+++ password.py
@@ -0,0 +1,39 @@
+import random
+import string
+# function that generates a single random character
+def random_character():
+    choices = string.ascii_letters + string.digits + string.punctuation
+    return random.choice(choices)
+
+passwordLength = 12 #make it so that ou ask the user for a lengh or generate a random number
+
+# function that generates a random password
+def generate_strong_password():
+    password = ""
+    for i in range(passwordLength):
+        password = password + random_character()
+    print (password)
+
+generate_strong_password()
+
+#function to give us a random word
+def fetch_word():
+    response = requests.get("https://random-word-api.herokuapp.com/word?length=6")
+    word = response.json()[0]
+    return word
+
+    def replaceLetters():
+        word = word[0].upper() + word[1:]
+
+        if "a" in word:
+            word = word.replace("a", "@")
+
+        return word
+    
+    def generate_weaker_password():
+        word1 = fetch_word()
+        word2 = fetch_word()
+        password = word1+word2
+        return password
+
+print(generate_weaker_password())
